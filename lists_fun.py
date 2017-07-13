c986c61a11d3257e4262e588adc60584e746b1d5
msliang <liangmichelles@gmail.com>
2017-07-13 11:51:30 -0400

    new projects

--- lists_fun.py
+++
@@ -1,8 +0,0 @@
-groceries= ["carrots","chips","pasta","cheese","cereal"]
-drinks=["juice","milk","soda"]
-print(groceries[len(groceries)-1])
-print(len(groceries))
-groceries.append("coffee")
-groceries.extend(drinks)
-for item in groceries:
-    print(str(groceries.index(item)+1)+":"+item)
